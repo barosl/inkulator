cf2bf3c55306d8c7c32f11f3232114fb473c403a
Barosl LEE <git@barosl.com>
2011-12-25 07:35:44 +0900

    clock should be initialized to 1
    Making the first rising edge occur after a cycle, not half a cycle.

--- uart_clk.v
+++ uart_clk.v
@@ -2,15 +2,15 @@ module uart_clk(tx_clk, rx_clk, rst, clk_50m);
 	output reg tx_clk, rx_clk;
 	input rst, clk_50m;
 
-	reg [8:0] cnt;
-
 	parameter CLKS_PER_BIT = 434; /* (1./115200)/(1./50/1000/1000) */
+
+	reg [8:0] cnt;
 
 	always @(posedge clk_50m or posedge rst) begin
 		if (rst) begin
 			cnt <= 0;
-			tx_clk <= 0;
-			rx_clk <= 0;
+			tx_clk <= 1;
+			rx_clk <= 1;
 
 		end else begin
 			cnt = (cnt + 1) % CLKS_PER_BIT;
